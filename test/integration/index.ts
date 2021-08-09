0956d3c94926977a38a49bb2a64f20408e9759af
Paul Razvan Berg <hello@paulrberg.com>
2021-08-09 13:26:58 +0300

    chore: archive repo

--- test/integration/index.ts
+++
@@ -1,6 +0,0 @@
-import { baseContext } from "../shared/contexts";
-import { integrationTestHifiPool } from "./hifiPool/HifiPool";
-
-baseContext("Integration Tests", function () {
-  integrationTestHifiPool();
-});
--- test/unit/index.ts
+++
@@ -1,8 +0,0 @@
-import { baseContext } from "../shared/contexts";
-import { unitTestHifiPool } from "./hifiPool/HifiPool";
-import { unitTestYieldSpace } from "./yieldSpace/YieldSpace";
-
-baseContext("Unit Tests", function () {
-  unitTestHifiPool();
-  unitTestYieldSpace();
-});
